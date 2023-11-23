6b43d2abfabbbe039d9c2a4e77f38f116196c627
anontheon <149826026+anontheon@users.noreply.github.com>
2023-11-23 13:51:44 -0300

    feat: packed address array functionality working

---
+++ src/PackedArray.sol
@@ -0,0 +1,289 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity >=0.8.21;
+
+import "forge-std/Test.sol";
+
+library PackedAddressArray {
+    error IndexOutOfBounds();
+    error InvalidIndexRange();
+    error PopEmptyArray();
+
+    struct Array {
+        uint256[] slots;
+    }
+
+    function append(Array storage arr, address[] memory addrs) internal {
+        assembly {
+            let arrayLen := sload(arr.slot)
+
+            let bitsAtStart := mul(arrayLen, 160)
+            let startSlot := div(bitsAtStart, 256)
+            let offset := mod(bitsAtStart, 256)
+
+            // calculate storage slot
+            mstore(0x00, arr.slot)
+            let storageSlot := keccak256(0x00, 0x20)
+            let storageSlotIndex := add(storageSlot, startSlot)
+            let cachedStorageValue := sload(storageSlotIndex)
+
+            let newAddrsLen := mload(addrs)
+            let i := 0
+
+            for {} lt(i, newAddrsLen) { i := add(i, 1) } {
+                let value := mload(add(add(addrs, 32), mul(i, 32)))
+
+                switch gt(offset, 96)
+                case 1 {
+                    // If the offset is greater than 96, we need to split the address across two slots
+                    let sliceLength := sub(160, sub(256, offset))
+                    sstore(storageSlotIndex, or(cachedStorageValue, shr(sliceLength, value)))
+                    storageSlotIndex := add(storageSlotIndex, 1)
+                    cachedStorageValue := shl(sub(256, sliceLength), value)
+                }
+                default {
+                    // If the offset is less than 96, we can fit the whole address in the slot
+                    cachedStorageValue := or(cachedStorageValue, shl(sub(96, offset), value))
+                }
+
+                offset := mod(add(offset, 160), 256)
+
+                // If next offset is 0, update storage slot beforehand
+                if iszero(offset) {
+                    sstore(storageSlotIndex, cachedStorageValue)
+                    storageSlotIndex := add(storageSlotIndex, 1)
+                    cachedStorageValue := sload(storageSlotIndex)
+                }
+            }
+
+            if gt(cachedStorageValue, 0) { sstore(storageSlotIndex, cachedStorageValue) }
+
+            // Increment array
+            sstore(arr.slot, add(newAddrsLen, arrayLen)) // increment array length
+        }
+    }
+
+    // Gets addresses starting at fromIndex..toIndex (does not include toIndex)
+    function getMany(Array storage arr, uint256 fromIndex, uint256 toIndex)
+        internal
+        view
+        returns (address[] memory addrs)
+    {
+        assembly {
+            let length := sload(arr.slot)
+
+            if gt(toIndex, length) {
+                mstore(0x00, 0x4e23d035) // IndexOutOfBounds()
+                revert(0x1c, 0x04)
+            }
+
+            if iszero(gt(toIndex, fromIndex)) {
+                mstore(0x00, 0x92f1b435) // InvalidIndexRange()
+                revert(0x1c, 0x04)
+            }
+
+            // for loop
+            let i := fromIndex
+            let numItems := sub(toIndex, fromIndex)
+            let bitsAtStart := mul(i, 160)
+            let startSlot := div(bitsAtStart, 256)
+            let offset := mod(bitsAtStart, 256)
+
+            // Allocate memory for the returned arrayLength
+            mstore(addrs, numItems)
+            let freeMemPtr := mload(0x40)
+            let arrayEnd := add(addrs, mul(numItems, 32))
+            // update free memory to end of array
+            mstore(0x40, add(arrayEnd, 32))
+
+            // Calculate storage spot for returned arrayLength
+            mstore(0x00, arr.slot)
+            let storageSlot := keccak256(0x00, 0x20)
+            let storageSlotIndex := add(storageSlot, startSlot)
+
+            // Cache beginning of array
+            let arrayPtr := addrs
+            // Cache raw slot value to avoid multiple sloads
+            let cachedStorageValue := sload(storageSlotIndex)
+
+            for {} lt(i, toIndex) { i := add(i, 1) } {
+                arrayPtr := add(arrayPtr, 32)
+                switch gt(offset, 96)
+                // If the offset is greater than 96, we need to get the address across two slots
+                case 1 {
+                    let sliceLength := sub(160, sub(256, offset))
+                    // Slice end of address from previous slot
+                    let highBits := shr(sub(offset, sliceLength), shl(offset, cachedStorageValue))
+                    // Move to next slot and grab the rest of the address
+                    storageSlotIndex := add(storageSlotIndex, 1)
+                    cachedStorageValue := sload(storageSlotIndex)
+                    let lowBits := shr(sub(256, sliceLength), cachedStorageValue)
+                    mstore(arrayPtr, or(highBits, lowBits))
+                }
+                // If the offset is less than 96 the address is fully in this slot
+                default { mstore(arrayPtr, shr(sub(96, offset), cachedStorageValue)) }
+
+                offset := mod(add(offset, 160), 256)
+
+                // If next offset is 0, update storage slot beforehand
+                if iszero(offset) {
+                    storageSlotIndex := add(storageSlotIndex, 1)
+                    cachedStorageValue := sload(storageSlotIndex)
+                }
+            }
+        }
+    }
+
+    // Push an address into the array
+    function push(Array storage arr, address value) internal {
+        assembly {
+            let numItems := sload(arr.slot)
+            let totalBitsUsed := mul(numItems, 160)
+            let slotIndex := div(totalBitsUsed, 256)
+            let offset := mod(totalBitsUsed, 256)
+
+            // Calculate storage spot for array
+            mstore(0x0, arr.slot)
+            let storageSlot := keccak256(0x0, 0x20)
+            let storageSlotIndex := add(storageSlot, slotIndex)
+
+            switch gt(offset, 96)
+            case 1 {
+                // If the offset is greater than 96, we need to split the address across two slots
+                let sliceLength := sub(160, sub(256, offset))
+                sstore(storageSlotIndex, or(sload(storageSlotIndex), shr(sliceLength, value)))
+                sstore(add(storageSlotIndex, 1), shl(sub(256, sliceLength), value))
+            }
+            default {
+                // If the offset is less than 96, we can fit the whole address in the slot
+                sstore(storageSlotIndex, or(sload(storageSlotIndex), shl(sub(96, offset), value)))
+            }
+            // Increment array
+            sstore(arr.slot, add(sload(arr.slot), 1)) // increment array length
+        }
+    }
+
+    // Remove last address from array and decrement length
+    function pop(Array storage arr) internal {
+        assembly {
+            // Calculate storage spot for array
+            mstore(0x0, arr.slot)
+            let storageSlot := keccak256(0x0, 0x20)
+            let numItems := sload(arr.slot)
+
+            if iszero(numItems) {
+                mstore(0x00, 0xcef37a85) // PopEmptyArray()
+                revert(0x1c, 0x04)
+            }
+
+            // calculate offset
+            let totalBitsUsed := mul(numItems, 160)
+            let slotIndex := div(totalBitsUsed, 256)
+            let offset := mod(totalBitsUsed, 256)
+
+            let indexToPop := add(storageSlot, slotIndex)
+
+            switch gt(offset, 159)
+            // If offset is greater than 159, the whole address should exist here
+            case 1 {
+                let rawSlotValue := sload(indexToPop)
+
+                // trim the last address + padding
+                let sliceLength := add(160, sub(256, offset))
+                let newValue := shl(sliceLength, shr(sliceLength, rawSlotValue))
+                sstore(indexToPop, newValue)
+            }
+            // The address to pop exists across two slots
+            default {
+                // The second slot can be zeroed out since nothing else should be there
+                sstore(indexToPop, 0)
+
+                let decrementedArraySlot := sub(indexToPop, 1)
+                let RawSlotValue := sload(decrementedArraySlot)
+
+                // Number of bits to trim off end of this slot
+                let sliceLength := sub(160, offset)
+                let newValue := shl(sliceLength, shr(sliceLength, RawSlotValue))
+                sstore(decrementedArraySlot, newValue)
+            }
+            // Decrement array length
+            sstore(arr.slot, sub(numItems, 1))
+        }
+    }
+
+    // Replace an addres in the array with new address value
+    function set(Array storage arr, uint256 index, address value) internal {
+        assembly {
+            let length := sload(arr.slot)
+
+            if iszero(gt(length, index)) {
+                mstore(0x00, 0x4e23d035) // IndexOutOfBounds()
+                revert(0x1c, 0x04)
+            }
+
+            let bitStart := mul(index, 160)
+            let startSlot := div(bitStart, 256)
+            let offset := mod(bitStart, 256)
+
+            // Calculate storage spot for array
+            mstore(0x0, arr.slot)
+            let storageSlot := keccak256(0x0, 0x20)
+            let storageSlotIndex := add(storageSlot, startSlot)
+
+            switch gt(offset, 96)
+            // Need to replace address across two slots
+            case 1 {
+                /*
+                let sliceLength := sub(160, sub(256, offset))
+                sstore(storageSlotIndex, or(and(sload(storageSlotIndex), shl(sub(256, sliceLength), value)), shr(sliceLength, value)))
+                sstore(add(storageSlotIndex, 1), shl(sub(256, sliceLength), value))
+                */
+
+                // TODO: how could this possibly be clearing the old bits?
+                let sliceLength := sub(160, sub(256, offset))
+                sstore(storageSlotIndex, or(sload(storageSlotIndex), shr(sliceLength, value)))
+                sstore(add(storageSlotIndex, 1), shl(sub(256, sliceLength), value))
+            }
+            // If the offset is less than 96, we can fit the whole address in the slot
+            default {
+                let sliceLength := sub(256, offset)
+                let cleanedSlot := shl(sliceLength, shr(sliceLength, sload(storageSlotIndex)))
+                let newValue := shl(sub(96, offset), value)
+                sstore(storageSlotIndex, or(cleanedSlot, newValue))
+            }
+        }
+    }
+
+    // Get an address from the array
+    function get(Array storage arr, uint256 index) internal view returns (address addr) {
+        assembly {
+            let length := sload(arr.slot)
+
+            if iszero(gt(length, index)) {
+                mstore(0x00, 0x4e23d035) // IndexOutOfBounds()
+                revert(0x1c, 0x04)
+            }
+
+            let bitStart := mul(index, 160)
+            let startSlot := div(bitStart, 256)
+            let offset := mod(bitStart, 256)
+
+            // Calculate storage spot for array
+            mstore(0x0, arr.slot)
+            let storageSlot := keccak256(0x0, 0x20)
+            let storageSlotIndex := add(storageSlot, startSlot)
+
+            switch gt(offset, 96)
+            case 1 {
+                // If the offset is greater than 96, we need to get the address across two slots
+                let sliceLength := sub(160, sub(256, offset))
+                let highBits := shr(sub(offset, sliceLength), shl(offset, sload(storageSlotIndex)))
+                let lowBits := shr(sub(256, sliceLength), sload(add(storageSlotIndex, 1)))
+                addr := or(highBits, lowBits)
+            }
+            default {
+                // If the offset is less than 96 the address is in this slot alone
+                addr := shr(sub(96, offset), sload(storageSlotIndex))
+            }
+        }
+    }
+}
---
+++ test/PackedArray.t.sol
@@ -0,0 +1,246 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/PackedArray.sol";
+
+contract PackedAddressArrayTest is Test {
+    using PackedAddressArray for PackedAddressArray.Array;
+
+    PackedAddressArray.Array internal arr;
+
+    function test_append_empty() public {
+        address[] memory addrs = new address[](8);
+
+        for (uint160 i = 0; i < 8; i++) {
+            addrs[i] = address(type(uint160).max - i);
+        }
+
+        arr.append(addrs);
+        assertEq(arr.slots.length, 8);
+
+        for (uint256 i = 0; i < 8; i++) {
+            assertEq(arr.get(i), addrs[i]);
+        }
+    }
+
+    function test_append_not_empty() public {
+        arr.push(address(0xCAFE));
+        arr.push(address(0xCAFE));
+        arr.push(address(0xCAFE));
+        uint256 lengthBefore = arr.slots.length;
+        address[] memory addrs = new address[](8);
+
+        for (uint160 i = 0; i < 8; i++) {
+            addrs[i] = address(type(uint160).max - i);
+        }
+
+        arr.append(addrs);
+        assertEq(arr.slots.length, 8 + lengthBefore);
+
+        for (uint256 i = lengthBefore; i < 8 + lengthBefore; i++) {
+            assertEq(arr.get(i), addrs[i - lengthBefore]);
+        }
+    }
+
+    function test_fuzz_all_functionality(address[30] calldata addrs, address a, address b) public {
+        address[] memory _addrs = new address[](addrs.length);
+        for (uint256 i = 0; i < addrs.length; i++) {
+            _addrs[i] = addrs[i];
+        }
+
+        // push and append
+        arr.push(a);
+        assertEq(arr.slots.length, 1);
+        assertEq(arr.get(0), a);
+
+        arr.append(_addrs);
+        assertEq(arr.slots.length, _addrs.length + 1);
+        for (uint256 i = 0; i < _addrs.length; i++) {
+            assertEq(arr.get(i + 1), _addrs[i]);
+        }
+
+        // remove last 1, and set all to address(0)
+        arr.pop();
+        assertEq(arr.slots.length, addrs.length);
+
+        for (uint256 i = 0; i < addrs.length; i++) {
+            arr.set(i, address(0));
+            assertEq(arr.get(i), address(0));
+        }
+        assertEq(arr.slots.length, addrs.length);
+        assertEq(_addrs.length, addrs.length);
+
+        // Get all zero addresses
+        address[] memory zeroAddrs = arr.getMany(0, addrs.length);
+        assertEq(zeroAddrs.length, addrs.length);
+        assertEq(arr.slots.length, addrs.length);
+
+        // TODO: why is this array being reset
+        // assertEq(_addrs.length, addrs.length);
+
+        // Remove all addresses
+        for (uint256 i = 0; i < addrs.length; i++) {
+            arr.pop();
+            console.log("pop length now", arr.slots.length);
+        }
+        vm.expectRevert(PackedAddressArray.PopEmptyArray.selector);
+        arr.pop();
+        assertEq(arr.slots.length, 0);
+
+        arr.push(b);
+        assertEq(arr.get(0), b);
+        assertEq(arr.slots.length, 1);
+        arr.set(0, a);
+        assertEq(arr.get(0), a);
+    }
+
+    function test_append_fuzz(address[20] calldata addrsA, address[20] memory addrsB) public {
+        address[] memory a = new address[](20);
+        address[] memory b = new address[](20);
+        for (uint256 i = 0; i < addrsA.length; i++) {
+            a[i] = addrsA[i];
+            b[i] = addrsB[i];
+        }
+        arr.append(a);
+        assertEq(arr.slots.length, addrsA.length);
+        for (uint256 i = 0; i < addrsA.length; i++) {
+            assertEq(arr.get(i), addrsA[i]);
+        }
+
+        arr.append(b);
+        assertEq(arr.slots.length, (addrsA.length + addrsB.length));
+
+        for (uint256 i = 0; i < addrsA.length; i++) {
+            assertEq(arr.get(i), addrsA[i]);
+        }
+
+        for (uint256 i = addrsA.length; i < addrsA.length + addrsB.length; i++) {
+            assertEq(arr.get(i), addrsB[i - addrsA.length]);
+        }
+    }
+
+    function test_set_debug() public {
+        address a = address(type(uint160).max);
+        address b = address(0xFF << 80);
+        for (uint256 i = 0; i < 10; i++) {
+            arr.push(a);
+        }
+        for (uint256 i = 0; i < 10; i++) {
+            console.logBytes32(bytes32(arr.slots[i]));
+        }
+        for (uint256 i = 0; i < 10; i++) {
+            arr.set(i, address(b));
+            assertEq(arr.get(i), address(b));
+        }
+        for (uint256 i = 0; i < 10; i++) {
+            console.logBytes32(bytes32(arr.slots[i]));
+        }
+    }
+
+    function test_fuzz_set(address[20] calldata addrsA, address[20] calldata addrsB) public {
+        for (uint256 i = 0; i < addrsA.length; i++) {
+            arr.push(addrsA[i]);
+        }
+
+        for (uint256 i = 0; i < addrsB.length; i++) {
+            // console.log("set", i, addrsA[i], addrsB[i]);
+            arr.set(i, addrsB[i]);
+            // console.log("get", i, arr.get(i));
+            assertEq(arr.get(i), addrsB[i]);
+        }
+
+        for (uint256 i = 0; i < addrsA.length; i++) {
+            arr.set(i, addrsA[i]);
+            assertEq(arr.get(i), addrsA[i]);
+        }
+    }
+
+    function test_fuzz_get_many(address[50] calldata array, uint256 from, uint256 to) public {
+        for (uint256 i = 0; i < array.length; i++) {
+            arr.push(array[i]);
+        }
+
+        if (to > array.length) {
+            vm.expectRevert(PackedAddressArray.IndexOutOfBounds.selector);
+        } else if (from >= to) {
+            vm.expectRevert(PackedAddressArray.InvalidIndexRange.selector);
+        }
+
+        address[] memory addrs = arr.getMany(from, to);
+        assertEq(addrs.length, to - from);
+
+        for (uint256 i = 0; i < addrs.length; i++) {
+            assertEq(addrs[i], array[from + i]);
+        }
+    }
+
+    function test_get_many_correct_amount() public {
+        arr.push(address(0xCAFE));
+        address[] memory addrs = arr.getMany(0, 1);
+        assertEq(addrs.length, 1);
+        arr.push(address(0));
+        addrs = arr.getMany(0, 2);
+        assertEq(addrs.length, 2);
+        addrs = arr.getMany(0, 1);
+        assertEq(addrs.length, 1);
+    }
+
+    function test_pop_empty() public {
+        vm.expectRevert(PackedAddressArray.PopEmptyArray.selector);
+        arr.pop();
+    }
+
+    function test_pop_functionality() public {
+        arr.push(address(0xCAFE));
+        arr.push(address(0xBEEF));
+        arr.pop();
+        assertEq(arr.slots.length, 1);
+
+        vm.expectRevert(PackedAddressArray.IndexOutOfBounds.selector);
+        arr.get(1);
+    }
+
+    function test_pop_sequence() public {
+        arr.push(address(0xCAFE));
+        arr.push(address(0xDECAF));
+        console.log("address[1]", arr.get(1));
+        assertEq(arr.get(1), address(0xDECAF));
+        arr.pop();
+        arr.pop();
+        arr.push(address(0xBEEF));
+        assertEq(arr.get(0), address(0xBEEF));
+    }
+
+    function test_push_pop(address[69] calldata addrs) public {
+        for (uint256 i = 0; i < addrs.length; i++) {
+            arr.push(addrs[i]);
+            assertEq(addrs[i], arr.get(i));
+        }
+        assertEq(arr.slots.length, addrs.length);
+
+        // pop all
+        for (uint256 i = 0; i < addrs.length; i++) {
+            arr.pop();
+        }
+        assertEq(arr.slots.length, 0);
+    }
+
+    function test_fuzz_push_get_single(address a, address b, address c) public {
+        arr.push(a);
+        arr.push(b);
+        arr.push(c);
+        assertEq(arr.get(0), a);
+        assertEq(arr.get(1), b);
+        assertEq(arr.get(2), c);
+    }
+
+    function test_fuzz_push_get_loop(address[200] calldata addrs) public {
+        for (uint256 i = 0; i < addrs.length; i++) {
+            arr.push(addrs[i]);
+        }
+        for (uint256 i = 0; i < addrs.length; i++) {
+            assertEq(arr.get(i), addrs[i]);
+        }
+    }
+}
